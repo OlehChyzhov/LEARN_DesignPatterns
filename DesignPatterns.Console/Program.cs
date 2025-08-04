ec8cb8b3df4145a1a0d6c2dff70e7d7a587fddf6
Олег Чижов <oleh.chyzhov@gmail.com>
2025-08-04 11:27:52 +0300

    Builder Design Pattern: Simple, Stepwise, Functional, Faceted

--- DesignPatterns.Console/Program.cs
+++ DesignPatterns.Console/Program.cs
@@ -1,9 +1,17 @@
-﻿using DesignPatterns.SOLID;
+﻿using DesignPatterns.Builder.Faceted_Builder;
+using DesignPatterns.Builder.Functional_Builder;
+using DesignPatterns.Builder.Solution;
+using DesignPatterns.Builder.Solution.Stepwise_Builder;
 
 public class Program
 {
     public static void Main()
     {
-        LSP_SolutionExample.Run();
+        //Director_Example.Run();
+        //StepwiseBuilder_Example.Run();
+
+        //FunctionalBuilder_Example.Run();
+
+        FacetedBuilder_Example.Run();
     }
 }
--- DesignPatterns.Console/SOLID/DependencyInversionPrinciple.cs
+++ DesignPatterns.Console/SOLID/DependencyInversionPrinciple.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DesignPatterns.SOLID
+﻿namespace DesignPatterns.SOLID
 {
     /// <summary>
     /// Dependency Inversion Principle (DIP) states:
